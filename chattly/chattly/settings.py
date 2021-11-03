9670b8307c4e9acd26e9ca38261a47d8107bab56
bartosz121 <bmagiera121@gmail.com>
2021-11-03 21:27:23 +0100

    Setting up asgi and channel layers

--- chattly/chattly/settings.py
+++ chattly/chattly/settings.py
@@ -130,3 +130,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ASGI_APPLICATION = "chattly.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
