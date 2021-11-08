f71e1f5b63b6a5d424f96f7278864970ec3d7884
bartosz121 <bmagiera121@gmail.com>
2021-11-08 22:43:41 +0100

    Add home view

--- chattly/chat/views.py
+++ chattly/chat/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    http_method_names = ["get"]
+    template_name = "chat/main.html"
