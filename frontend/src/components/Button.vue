b1df82a2159074bf0d58d4f28ae196c667ca9750
bartosz121 <bmagiera121@gmail.com>
2021-11-14 15:45:06 +0100

    Add `Button` component

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+    <button @click="onClick()" :class="className" :type="this.type">
+        <slot />
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        type: {
+            type: String,
+            default: 'button'
+        },
+        bg: {
+            type: String,
+            default: "bg-sky-500 hover:bg-sky-600"
+        },
+        textColor: {
+            type: String,
+            default: "text-white"
+        },
+        sizing: {
+            type: String,
+            default: "py-2 px-4 rounded"
+        },
+        extendStyle: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        onClick(){
+            this.$emit('btn-click')
+        }
+    },
+    computed: {
+        className() {
+            return `${this.bg} ${this.textColor} ${this.sizing} ${this.extendStyle}`
+        },
+    }
+}
+</script>
