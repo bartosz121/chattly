d7dbcb0e6c4810487fe54e40f2682014ab979cbb
bartosz121 <bmagiera121@gmail.com>
2021-12-11 23:20:53 +0100

    Add PeerStream component

---
+++ frontend/src/components/PeerStream.vue
@@ -0,0 +1,18 @@
+<template>
+  <video
+    class="h-full"
+    :srcObject="this.peerObj.remoteStream"
+    :id="this.peerObj.peerName"
+    autoplay
+    playsinline
+  ></video>
+</template>
+
+<script>
+export default {
+  name: "PeerStream",
+  props: {
+    peerObj: Object,
+  },
+};
+</script>
