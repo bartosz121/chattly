68444d62d348cf21f3515cf1f6bb734d0785a710
bartosz121 <bmagiera121@gmail.com>
2021-12-20 22:13:36 +0100

    Add tailwind classes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div
+    class="
+      flex flex-col
+      gap-4
+      md:gap-6 md:flex-row
+      justify-center
+      items-center
+      h-screen
+      bg-gradient-to-tl
+      from-blue-300
+      to-blue-100
+    "
+  >
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  components: {},
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
